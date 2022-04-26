272468508bf2e44ccd3b9e87c2cd4d1856cc1485
Théo Dupont <theo.dupont@etu.u-bordeaux.fr>
2022-04-26 17:42:16 +0200

    Ajout styled components et hover sur les drapeaux

--- src/componants/CountryCadre/styles.js
+++ src/componants/CountryCadre/styles.js
@@ -1,8 +1,44 @@
-import { makeStyles } from "@mui/styles";
+import { Grid } from "@mui/material";
+import styled from "styled-components";
+
+export const CountryOverLayer = styled.div({
+  display: "none",
+  position: "absolute",
+  backgroundColor: "rgba(0,0,0, .5)",
+  top: 32,
+  height: "calc(100% - 32px)",
+  width: "calc(100% - 32px)",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
+export const CountryCadreBlock = styled(Grid)({
+  position: "relative",
+  height: "",
+  /* &: Element courant. On donne ici à un element enfant (ici CountryOverLayer) un couple de clé/valeur à changer lors d'un hover */
+  [`&:hover > ${CountryOverLayer}`]: {display:"flex"}
+});
 
 
-export default makeStyles(() => ({
-    cadre: {
-        //TO DO Hover pour afficher les données
-    }
-}))
+
+export const CountryFlag = styled.img({
+  width: "100%",
+  //Si on veut faire en sorte que chaque drapeau remplisse l'espace attitré, on doit faire couvrir l'image car pas tous le même format.
+  height: "100%",
+  objectFit: "cover",
+  objectPosition: "center",
+});
+
+export const CountryOverLayerText = styled.span({
+  fontSize: 13,
+  color: "#fff",
+  margin: "15px 0",
+  fontFamily:"Helvetica",
+});
+
+// export default makeStyles(() => ({
+//   cadre: {
+//     //TO DO Hover pour afficher les données
+//   },
+// }));
